dd9b8e5f4163c0a2e9111637281065e217ebbc0c
SubhSarkar <datacoot@gmail.com>
2019-08-01 14:45:22 +0530

    Reading the data for the Titanic problem

--- titanic_log.py
+++ titanic_log.py
@@ -20,3 +20,28 @@ from sklearn.tree import DecisionTreeClassifier
 
 # importing the metrics class
 from sklearn import metrics
+
+#Using a magic function to print all the plots inline
+%matplotlib inline
+
+#reading the data through a csv file
+
+titanic_train = pd.read_csv('C:/Users/Anupriya/OneDrive/Work/Self Study/Kaggle/Titanic/train.csv')
+titanic_test = pd.read_csv('C:/Users/Anupriya/OneDrive/Work/Self Study/Kaggle/Titanic/test.csv')
+
+# Data Exploration and dataset understanding
+# First we will expand the output display to see more columns
+
+pd.set_option('display.expand_frame_repr', False)
+
+summarystat = titanic_train.describe(include = 'all')
+print(summarystat)
+
+# Data type of different variable in the dataframe
+
+titanic_train.info()
+
+titanic_train.head(5)
+titanic_train.count()
+
+#Cabin (204), age (714), and embarked (889) have missing data
